02d12588f198449ba2c0e10b3f1112a5f8c93e38
AdaEbicom <87523530+AdaEbicom@users.noreply.github.com>
2021-07-26 13:16:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 
 @app.route('/hello', methods = ['POST'])
 def hello():
+    print("a")
     #data = request.get_json(silent=True)
-    return {"lalala"}, 200   
+    return 200   
 
