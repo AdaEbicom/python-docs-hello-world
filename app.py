03bbce6db3143afed9689efec09e1c7f366f5bee
AdaEbicom <87523530+AdaEbicom@users.noreply.github.com>
2021-07-26 12:39:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello', methods = ['GET', 'POST'])
+@app.route('/hello', methods = ['GET'])
 def hello():
     if request.method == 'GET':
-        return "Hello, Azure GET!"
-    
-    else if request.method == 'POST':
-        return "Hello, Azure POST!"
-    
+        return "Hello, Azure GET!"    
     else:
         return {}, 405
