1fadbe826f4bc4bfd5cbe31444bf5e7d43e5841a
AdaEbicom <87523530+AdaEbicom@users.noreply.github.com>
2021-07-28 10:16:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,7 @@ import pandas as pd
 import json
 from types import SimpleNamespace
 
-from pvlib.pvsystem import PVSystem #, retrieve_sam
-from pvlib.temperature import TEMPERATURE_MODEL_PARAMETERS
-from pvlib.modelchain import ModelChain
-from pvlib.location import Location
-
-############# funkcja
-def pvlib_forecast(lat, lon, tz, mod_pdc0, mod_gamma_pdc, inv_pdc0, weather):
-
-    temperature_model_parameters = TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
-    location = Location(lat, lon, tz)
-    
-    pvwatts_system = PVSystem(
-        module_parameters={'pdc0': mod_pdc0, 'gamma_pdc': mod_gamma_pdc},
-        inverter_parameters={'pdc0': inv_pdc0},
-        temperature_model_parameters=temperature_model_parameters)
-    
-    mc = ModelChain(pvwatts_system, location,
-                    aoi_model='no_loss', spectral_model='no_loss')
-    
-    mc.run_model_from_effective_irradiance(weather)
-    
-    return(mc.ac)
-############## koniec funkcji
+import forecastfun
 
 app = Flask(__name__)
 
@@ -40,7 +18,7 @@ def forecast():
     weather = pd.DataFrame.from_dict(weather)   
     weather.set_index('dt', inplace = True)        
         
-    result = pvlib_forecast(d.location.latitude, 
+    result = forecastfun.pvlib_forecast(d.location.latitude, 
                                   d.location.longitude,
                                   d.location.timezone, 
                                   d.param.mod_pdc0, 
